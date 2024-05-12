6bc0d3ac44c73e432ed6f7812fc9460f199fdec0
nazmul-1117 <nazmulonline2002@gmail.com>
2024-05-12 16:24:52 +0600

    V0.4

--- Object_Oriented_Programming/MediOxide-An ePrescription Management System/src/main/java/medioxide/controller/HomeController.java
+++ Object_Oriented_Programming/MediOxide-An ePrescription Management System/src/main/java/medioxide/controller/HomeController.java
@@ -1,12 +1,10 @@
 package medioxide.controller;
 
-import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import medioxide.java.Main;
 
@@ -14,7 +12,6 @@ import java.io.IOException;
 
 public class HomeController {
 
-    private JFXButton switchToHome;
     private Stage stage;
     private Scene scene;
     private FXMLLoader fxmlLoader;
@@ -22,7 +19,22 @@ public class HomeController {
     public void buttonSwitch(ActionEvent event) {
     }
 
-    public void prescriptionButtonClicked(ActionEvent event){
+    @FXML public void prescriptionButtonClicked(ActionEvent event){
+        String clickedOption = event.getSource().toString();
+        System.out.println("Prescription Button Clicked..!\n"+clickedOption);
 
+        fxmlUrl = "prescription.fxml";
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlUrl));
+        try {
+            scene = new Scene(fxmlLoader.load());
+        }catch (IOException ioException){
+            System.out.println("FXML Load Fail\n"+ioException);
+        }finally {
+            stage.setScene(scene);
+            stage.show();
+        }
+
+        System.out.println("prescription exicute");
     }
 }
